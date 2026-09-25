e715d037abba64d55e9ca9835029731441f6916e
ashleyruelas5083 <ashley.ruelas5083@alumnos.udg.mx>
2026-09-24 21:04:08 -0600

    Update manzanita.py

--- practicas/manzanita.py
+++ practicas/manzanita.py
@@ -1,1 +1,17 @@
+import time
 
+print("Hola, ayúdame a recolectar manzanas")
+time.sleep(4)
+
+respuesta = input("¿Siiiiiii? ")
+
+print("[Siguiente disfraz]")
+
+print("De acuerdo... ¡Vamos!")
+time.sleep(2)
+
+print("[Siguiente disfraz]")
+
+while True:
+    print("[El personaje sigue al puntero del ratón]")
+    time.sleep(1)
